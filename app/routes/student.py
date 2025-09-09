99985dd2c9b031690837e99516dbc32b6da5105f
Vanshika Ramchandani <vanshikaramchandani8@gmail.com>
2025-09-09 23:27:41 +0530

    Student Pages UI

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,5 +1,5 @@
 # app/routes/student.py
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 bp = Blueprint('student', __name__)
@@ -10,3 +10,15 @@ def dashboard():
     if current_user.role != 'student':
         return redirect(url_for('main.home'))
     return render_template('student/dashboard.html')
+
+@bp.route('/modules')
+def modules():
+    return render_template('student/modules.html')
+
+@bp.route('/drills')
+def drills():
+    return render_template('student/drill.html')
+
+@bp.route('/incident')
+def incident():
+    return render_template('student/incident.html')
